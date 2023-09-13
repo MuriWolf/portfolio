aa1192e520c119627b6ce34945f6ceeccd68f6e7
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2023-09-13 15:58:07 -0300

    hard reset

--- src/js/modules/changeHeader.js
+++
@@ -1,9 +0,0 @@
-export default function changeHeader(header) {
-    setTimeout(()=> {
-        if (scrollY > 200) {
-            header.classList.remove("header-lg");
-        } else {
-            header.classList.add("header-lg");
-        }
-    }, 400)
-}
