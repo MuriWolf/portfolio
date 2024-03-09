f9c4844e9a01d3eb0d81f9e8d19943b2909d1b52
Murillo Pinheiro de Oliveira <murillop.o06@gmail.com>
2024-03-09 19:46:52 -0300

    test: env fix

--- src/lib/layout/Contact.svelte
+++ src/lib/layout/Contact.svelte
@@ -1,5 +1,4 @@
 <script>
-
 	import Button from "$lib/components/ui/button/button.svelte";
 
 </script>
--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -1,9 +1,10 @@
-import { SECRET_API_KEY } from '$env/static/private'
+// import { SECRET_API_KEY } from '$env/static/private'
+import { env } from '$env/dynamic/private'
 import projects from "../../../../data.json";
 
 export function GET({ request }) {                              
     const authHeader = request.headers.get("Authorization");
-    if (authHeader != SECRET_API_KEY) {
+    if (authHeader != env.SECRET_API_KEY) {
         return new Response(JSON.stringify({ message: 'Invalid Credencials' }), { status: 401 })
     }
     return new Response(JSON.stringify(projects), { status: 200 })
